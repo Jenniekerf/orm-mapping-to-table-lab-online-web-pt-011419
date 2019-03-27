95142397773b295af45c44547eff2fe3c2589a84
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-27 22:14:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,10 @@ class Student
   def.create_table
   
   end
+  
+  def self.drop_table
+    
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
